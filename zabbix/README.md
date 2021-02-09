89517dbf73c12063b06d6c3459ea40483de85b6c
m11k08 <bear.kohei@gmail.com>
2021-02-09 14:49:36 +0900

    zabbix agent added

---
+++ zabbix/README.md
@@ -0,0 +1,13 @@
+# zabbix
+
+## Install zabbix-server
+
+Follow this web page:
+https://qiita.com/zembutsu/items/d98099bf68399c56c236
+
+## Install zabbix-agent
+
+Follow this web page:
+https://qiita.com/ymfj/items/f3d2b36f9ad9cdc599c3
+
+zabbix/install_agent.sh is shell script to install zabbix-agent to client pc following web page.
---
+++ zabbix/install_agent.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ZBX_SRV="dollar"
+# install zabbix-server
+sudo apt update && sudo apt install -y zabbix-agent
+# rewrite config
+sudo sed -e "s/Server=127.0.0.1/Server=$ZBX_SRV/g" -e "s/ServerActive=127.0.0.1/ServerActive=$ZBX_SRV/g" -e "s/Hostname=Zabbix server/Hostname=${HOST}/g" /etc/zabbix/zabbix_agentd.conf > /etc/zabbix/zabbix_agentd.conf
+sudo systemctl enable zabbix-agent
+# setup for GPU
+if [ -e /proc/driver/nvidia/version ] ; then
+    if [ ! -d zabbix-nvidia-smi-multi-gpu ] ; then
+        git clone https://github.com/plambe/zabbix-nvidia-smi-multi-gpu.git
+    fi
+    sudo cat zabbix-nvidia-smi-multi-gpu/userparameter_nvidia-smi.conf.linux >> /etc/zabbix/zabbix_agentd.conf
+    if [ ! -d /etc/zabbix/scripts ] ; then
+        sudo mkdir /etc/zabbix/scripts
+    fi
+    sudo cp zabbix-nvidia-smi-multi-gpu/get_gpus_info.sh /etc/zabbix/scripts/
+    sudo chmod +x /etc/zabbix/scripts/get_gpus_info.sh
+fi
