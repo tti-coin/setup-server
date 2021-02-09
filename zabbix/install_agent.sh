f7b01c796107321a12be57ef3a025cf1dad84d5b
m11k08 <bear.kohei@gmail.com>
2021-02-09 15:47:59 +0900

    bug fixed

--- zabbix/install_agent.sh
+++ zabbix/install_agent.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 ZBX_SRV="dollar"
+HOST=`hostname`
 # install zabbix-server
 sudo apt update && sudo apt install -y zabbix-agent
 # rewrite config
-sudo sed -e "s/Server=127.0.0.1/Server=$ZBX_SRV/g" -e "s/ServerActive=127.0.0.1/ServerActive=$ZBX_SRV/g" -e "s/^Hostname=Zabbix server/Hostname=${HOST}/g" zabbix/zabbix_agentd.conf.master > /etc/zabbix/zabbix_agentd.conf
+sudo sed -e "s/Server=127.0.0.1/Server=$ZBX_SRV/g" -e "s/ServerActive=127.0.0.1/ServerActive=$ZBX_SRV/g" -e "s/^Hostname=.*?$/Hostname=${HOST}/g" zabbix/zabbix_agentd.conf.master > /etc/zabbix/zabbix_agentd.conf
 sudo systemctl enable zabbix-agent.service
 # setup for GPU
 if [ -e /proc/driver/nvidia/version ] ; then
